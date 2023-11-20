9bbf6101dd6d3165479db37d725c29ed914c2b8d
lucidrains <lucidrains@gmail.com>
2023-11-20 08:25:29 -0800

    scaffold

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'mirasol-pytorch',
+  packages = find_packages(exclude=[]),
+  version = '0.0.1',
+  license='MIT',
+  description = 'Mirasol - Pytorch',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  long_description_content_type = 'text/markdown',
+  url = 'https://github.com/lucidrains/mirasol-pytorch',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'adaptive computation'
+  ],
+  install_requires=[
+    'einops>=0.7.0',
+    'torch>=2.0'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
