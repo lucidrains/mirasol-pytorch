7f1c70fb0a53282a3fb08eff7f6a510639e914f0
lucidrains <lucidrains@gmail.com>
2023-11-22 11:05:29 -0800

    combiner is another transformer

--- mirasol_pytorch/mirasol_pytorch.py
+++ mirasol_pytorch/mirasol_pytorch.py
@@ -39,6 +39,8 @@ class Mirasol(Module):
         text_max_seq_len = 2048,
         encoder_depth = 6,
         decoder_depth = 6,
+        combiner_depth = 2,
+        combiner_output_num_tokens = 3,
         attn_dim_head = 64,
         attn_heads = 8,
         attn_layers_kwargs: dict = dict(),
@@ -49,11 +51,23 @@ class Mirasol(Module):
     ):
         super().__init__()
 
-        # rotary embedding
+        # combiner, which they found another transformer (followed by splicing the output) is sufficient
+
+        self.combiner = Encoder(
+            dim = dim,
+            depth = combiner_depth,
+            dim_head = attn_dim_head,
+            heads = attn_heads,
+            *attn_layers_kwargs
+        )
+
+        self.combiner_output_num_tokens = combiner_output_num_tokens
+
+        # a/v rotary embedding
 
         self.audio_video_rotary_pos_emb = RotaryEmbedding(attn_dim_head)
 
-        # audio + video encoder
+        # a/v encoder
 
         self.encoder = Encoder(
             dim = dim,
