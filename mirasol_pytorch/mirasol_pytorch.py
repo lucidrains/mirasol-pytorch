a37d007f859b7d04357e055d1081a8e1b5a21eab
lucidrains <lucidrains@gmail.com>
2023-11-22 10:43:42 -0800

    some more prep, the relative positions for the audio video encoder will need to be custom constructed and passed into attention layers, as it is done chunk-wise

--- mirasol_pytorch/mirasol_pytorch.py
+++ mirasol_pytorch/mirasol_pytorch.py
@@ -3,30 +3,97 @@ import torch.nn.functional as F
 from torch import Tensor, nn, einsum
 from torch.nn import Module, ModuleList
 
-from x_transformers import Encoder, Decoder
-
 from beartype import beartype
-
 from einops import rearrange, repeat
+
+from x_transformers import (
+    Encoder,
+    Decoder,
+    TransformerWrapper,
+    AutoregressiveWrapper
+)
+
+from x_transformers.x_transformers import RotaryEmbedding
 
 # helper functions
 
 def exists(v):
     return v is not None
 
+def default(*args):
+    for arg in args:
+        if exists(arg):
+            return arg
+    return None
+
 # main class
 
 class Mirasol(Module):
+
+    @beartype
     def __init__(
-        self
+        self,
+        *,
+        dim,
+        num_text_tokens,
+        text_max_seq_len = 2048,
+        encoder_depth = 6,
+        decoder_depth = 6,
+        attn_dim_head = 64,
+        attn_heads = 8,
+        attn_layers_kwargs: dict = dict(),
+        autoregressive_wrapper_kwargs: dict = dict(
+            pad_value = 0,
+            ignore_index = -100
+        )
     ):
         super().__init__()
 
+        # rotary embedding
+
+        self.audio_video_rotary_pos_emb = RotaryEmbedding(attn_dim_head)
+
+        # audio + video encoder
+
+        self.encoder = Encoder(
+            dim = dim,
+            depth = encoder_depth,
+            dim_head = attn_dim_head,
+            heads = attn_heads,
+            **attn_layers_kwargs
+        )
+
+        # text decoder
+
+        self.decoder = TransformerWrapper(
+            num_tokens = num_text_tokens,
+            max_seq_len = text_max_seq_len,
+            attn_layers = Decoder(
+                dim = dim,
+                depth = decoder_depth,
+                dim_head = attn_dim_head,
+                heads = attn_heads,
+                rotary_pos_emb = True,
+                **attn_layers_kwargs
+            )
+        )
+
+        self.wrapped_decoder = AutoregressiveWrapper(
+            self.decoder,
+            **autoregressive_wrapper_kwargs
+        )
+
+    @beartype
     def forward(
         self,
         *,
         audio: Tensor,
         video: Tensor,
-        text: Tensor
+        text: Tensor,
+        return_loss = True
     ):
-        return 0.
+
+        if not return_loss:
+            return self.decoder(text)
+
+        return self.wrapped_decoder(text)
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
   install_requires=[
     'beartype',
     'einops>=0.7.0',
-    'x-transformers>=1.25.2',
+    'x-transformers>=1.25.4',
     'torch>=2.0'
   ],
   classifiers=[
